146a463f7c807c66dac70a40ecd4c8795ff97de9
Amit Chandravadiya <amitckn6@gmail.com>
2024-02-11 19:00:16 +0530

    created database and signup table in sql server

---
+++ med_db.sql
@@ -0,0 +1,5 @@
+CREATE DATABASE med_signup_form;
+
+USE med_signup_form;
+
+SELECT DATABASE();
---
+++ med_signup_db.sql
@@ -0,0 +1,10 @@
+CREATE TABLE signup (
+    First_Name varchar(50),
+    Last_Name varchar(50),
+    Gender varchar(2),
+    User_Name varchar(20),
+    email varchar(50),
+    passcode varchar(64)
+);
+
+
