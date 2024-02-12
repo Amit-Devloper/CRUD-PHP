907a88e6d76d635e4ca9f07c9d79e4f19bda8972
Amit Chandravadiya <amitckn6@gmail.com>
2024-02-12 07:36:04 +0530

    added one sr_no column in the signup table

--- med_signup_db.sql
+++ med_signup_db.sql
@@ -1,10 +1,12 @@
 CREATE TABLE signup (
+    Sr_No INT NOT NULL AUTO_INCREMENT,
     First_Name varchar(50),
     Last_Name varchar(50),
     Gender varchar(2),
     User_Name varchar(20),
     email varchar(50),
-    passcode varchar(64)
+    passcode varchar(64),
+    PRIMARY KEY (Sr_No)
 );
 
 
